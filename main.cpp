6801eac4fb2d67832d50118875667d45fffa975d
david <david.berger@heig-vd.ch>
2022-09-27 15:31:49 +0200

    cout case

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@ int main() {
 
     int caseA1, caseA2, caseA3, caseB1, caseB2, caseB3, caseC1, caseC2, caseC3;
 
+    cout << "Quelle case voulez-vous jouer ?" << endl;
 
     cout << "TicTacToe!" << endl;
-
     return 0;
 
 }
